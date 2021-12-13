9f36ed3ff26a71f754a84b1dc1e6a51028ac4df9
曾楷翔 <31460809+okzkx@users.noreply.github.com>
2021-12-13 14:53:01 +0800

    53. CameraCapture

--- Assets/Samples/53. CameraCapture/CameraCapture.cs
+++ Assets/Samples/53. CameraCapture/CameraCapture.cs
@@ -2,16 +2,22 @@ using UnityEngine;
 using UnityEngine.Rendering.HighDefinition;
 using UnityEngine.Rendering;
 
-class DepthCapture : CustomPass {
+class CameraCapture : CustomPass {
+    public Camera bakingCamera;
+
     public RenderTexture depthTexture = null;
     public RenderTexture normalTexture = null;
     public RenderTexture tangentTexture = null;
 
     protected override bool executeInSceneView => false;
 
-    protected override void Execute(CustomPassContext ctx) {
-        Camera bakingCamera = ctx.hdCamera.camera;
+    protected override void Setup(ScriptableRenderContext renderContext, CommandBuffer cmd) {
+        if (bakingCamera == null) {
+            bakingCamera = Camera.main;
+        }
+    }
 
+    protected override void Execute(CustomPassContext ctx) {
         // We need to be careful about the aspect ratio of render textures when doing the culling, otherwise it could result in objects poping:
         {
             float aspect = bakingCamera.aspect;
@@ -39,15 +45,18 @@ class DepthCapture : CustomPass {
         var overrideDepthTest = new RenderStateBlock(RenderStateMask.Depth) {depthState = new DepthState(true, CompareFunction.LessEqual)};
 
         // Depth
-        if (depthTexture != null)
+        if (depthTexture != null) {
             CustomPassUtils.RenderDepthFromCamera(ctx, bakingCamera, depthTexture, ClearFlag.Depth, bakingCamera.cullingMask, overrideRenderState: overrideDepthTest);
+        }
 
         // Normal
-        if (normalTexture != null)
+        if (normalTexture != null) {
             CustomPassUtils.RenderNormalFromCamera(ctx, bakingCamera, normalTexture, ClearFlag.All, bakingCamera.cullingMask, overrideRenderState: overrideDepthTest);
+        }
 
         // Tangent
-        if (tangentTexture != null)
+        if (tangentTexture != null) {
             CustomPassUtils.RenderTangentFromCamera(ctx, bakingCamera, tangentTexture, ClearFlag.All, bakingCamera.cullingMask, overrideRenderState: overrideDepthTest);
+        }
     }
 }
