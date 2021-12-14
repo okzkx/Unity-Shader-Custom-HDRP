701935c9a0910332daf2e0c5bff12970ca998526
曾楷翔 <31460809+okzkx@users.noreply.github.com>
2021-12-14 15:47:14 +0800

    Update OverrideCamera.cs

--- Assets/Samples/52. OverrideCamera/OverrideCamera.cs
+++ Assets/Samples/52. OverrideCamera/OverrideCamera.cs
@@ -15,22 +15,7 @@ public class OverrideCamera : CustomPass {
     protected override bool executeInSceneView => false;
 
     protected override void AggregateCullingParameters(ref ScriptableCullingParameters cullingParameters, HDCamera hdCamera) {
-        Camera currentCam = hdCamera.camera;
-        if (currentCam.cameraType == CameraType.SceneView) {
-            return;
-        }
-
         cullingParameters.cullingMask |= (uint) overrideMask.value;
-
-        // 没找到方法能单独设置不对 overrideMask 层进行视锥剔除
-        // 临时方案是让物体始终在 MainCamera 前方，或者调大 Bounding Box，以至于不被主相机剔除掉
-        // Doesn't work 
-        // cullingParameters.isOrthographic = true;
-        // cullingParameters.cullingMatrix = Matrix4x4.Ortho(-99999, 99999, -99999, 99999, 0.001f, 99999) *
-        //                                   Matrix4x4.Translate(Vector3.forward * -99999 / 2f) *
-        //                                   hdCamera.camera.worldToCameraMatrix;
-        // Doesn't work either
-        // cullingParameters.cullingOptions = CullingOptions.None;
     }
 
     protected override void Setup(ScriptableRenderContext renderContext, CommandBuffer cmd) {
@@ -40,11 +25,6 @@ public class OverrideCamera : CustomPass {
     }
 
     protected override void Execute(CustomPassContext ctx) {
-        Camera currentCam = ctx.hdCamera.camera;
-        if (currentCam.cameraType == CameraType.SceneView) {
-            return;
-        }
-
         if (overrideCamera == null) {
             CreateOverrideCamera();
         }
